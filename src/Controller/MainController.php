247bc831cf3e26f14aeb2847aa51bdfe50cad719
Ronen Amon <ronenamon88@gmail.com>
2021-04-17 00:11:32 +0300

    add bootstrap navbar

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,11 +18,36 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        $number = random_int(0, 100);
+        $ideas = [
+        [   "id"=>1,
+            "title"=>"Title A",
+            "content"=>" 4x4 4x4 "
+        ],
+        [   "id"=>2,
+            "title"=>"Title B",
+            "content"=>" 4x4 4x4 "
+        ],
+        [   "id"=>3,
+            "title"=>"Title C",
+            "content"=>" 4x4 4x4 "
+        ],
+        [   "id"=>4,
+            "title"=>"Title D",
+            "content"=>" 4x4 4x4 "
+        ],
+        [   "id"=>5,
+            "title"=>"Title E",
+            "content"=>" 4x4 4x4 "
+        ],
+        [   "id"=>6,
+            "title"=>"Title F",
+            "content"=>" 4x4 4x4 "
+        ]
+        ];
 
         // return new Response(
         //     '<html><body>Lucky number: '.$number.'</body></html>'
         // );
-        return $this->render('ideas/index.html.twig');
+        return $this->render('ideas/index.html.twig',array("data"=>$ideas));
     }
 }
